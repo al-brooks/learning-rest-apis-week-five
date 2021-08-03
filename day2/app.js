d6258457fc752d21cf9bbce8e7eb6d8b3b7b3bdc
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-08-03 14:49:28 -0400

    Coffee Order App - HTML Layout

---
+++ day2/app.js
@@ -0,0 +1,33 @@
+// In-progress: Coffee Orders App
+
+/* 
+    Nav Bar - Get Orders by email address:
+        id="searchEmailText"
+        id="saveBtn"
+    
+    Create Orders
+        id="orderEmailText"
+        id="orderType"
+        id="orderSize"
+        id="orderQuantity"
+        id="submitOrder"
+    
+    Delete Orders
+        id="deleteEmailText"
+        id="deleteOrderBtn
+    
+    Order Display Section
+        id="ordersUL
+
+
+// To-do: Create variables for HTML element
+
+// To-do: Get all orders - display on webpage
+
+// To-do: Create new order
+
+// To-do: Get order by email
+
+// To-do: Delete Order by email
+
+*/
