6bc389bc1bc9b208966c05b6bcc133b1735e0169
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-08-04 14:31:55 -0400

    weather api app

---
+++ day3/app.js
@@ -0,0 +1,8 @@
+// to-do: Get weather
+// display main.temp_min, main.temp_max,main.pressure, and name
+
+// note: need to replace Chicago with city var
+const WEATHER_URL =
+  'api.openweathermap.org/data/2.5/weather?q=chicago&appid=45707788eec4bfd8a60d8a7d7afade47&units=imperial';
+
+// to-do: Fetch API
